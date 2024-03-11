2a6f9e427e0c2cd50c4fe566ae47f95ac4da3b05
Kang-Li (Stephen) Cheng <kangli.cheng@gmail.com>
2024-03-11 11:30:42 -0700

    add stock route app.py

--- app.py
+++ app.py
@@ -4,3 +4,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+    
+@app.route('/stock/<ticker>')
+def get_stock_data(ticker):
+    if ticker == "AAPL":
+        return jsonify({"name": "Apple Inc.", "last_price": 181.30, "ticker": "AAPL", "market_cap": "2.797T"})
+
+    if ticker == "META":
+        return jsonify({"name": "Meta Platforms, Inc.", "last_price": 481.74, "ticker": "META", "market_cap": "1.228T"})
+
+    if ticker == "COIN":
+        return jsonify({"name": "Coinbase Global, Inc.", "last_price": 193.94, "ticker": "COIN", "market_cap": "46.987B"})
+
+    if ticker == "SMCI":
+        return jsonify({"name": "Super Micro Computer, Inc.", "last_price": 895.93, "ticker": "SMCI", "market_cap": "46.987B"})
