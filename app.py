12e4774fefcd882576a139568007899501cbafa6
kanglicheng <kangli.cheng@gmail.com>
2024-03-11 20:54:21 -0700

    add all_stocks

--- app.py
+++ app.py
@@ -24,3 +24,13 @@ def get_stock_data(ticker):
         return jsonify({"name": "Super Micro Computer, Inc.", "last_price": 895.93, "ticker": "SMCI", "market_cap": "46.987B"})
     if ticker == "NOW":
         return jsonify({"name": "Service Now Inc", "last_price": 732.66, "ticker": "NOW", "market_cap": "98B"})
+    if ticker == "NVDA":
+        return jsonify({"name": "Nvidia", "last_price": 832.86, "ticker": "NVDA", "market_cap": "2.1T"})
+    if ticker == "AMD":
+        return jsonify({"name": "AMD", "last_price": 196.78, "ticker": "AMD", "market_cap": "333B"})
+
+
+@app.route('/all_stocks')
+def get_all_symbols():
+
+    return jsonify({"all_stocks": ["AAPL", "META", "COIN", "SMCI", "NOW", "AMD", "NVDA"]})
